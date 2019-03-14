36292bae3b69d34b562931bd0b3f126aff6d5a40
sentiahn <app.smartahn@gmail.com>
2019-03-13 22:57:30 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,219 @@
+/*!
+ * hbs-mailer
+ * Copyright(c) 2019 Junmin Ahn
+ * MIT Licensed
+ */
+
+'use strict'
+
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+const Handlebars = require('handlebars');
+const htmlToText = require('html-to-text');
+
+const isArray = Array.isArray;
+
+let _smtpTransport;
+let _locals;
+
+const _options = {
+  cache: false,
+};
+
+const _templates = Object.create(null);
+const _cached = Object.create(null);
+
+/**
+ * Helpers
+ */
+const isFunction = function(value) {
+  return typeof value === 'function';
+};
+
+const readFileAsync = function(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, function(err, content) {
+      if (err) return reject(err)
+      return resolve(content.toString());
+    })
+  });
+};
+
+/**
+ * Set the mailer transport options.
+ */
+const createTransport = function(options) {
+  _smtpTransport = nodemailer.createTransport(options);
+  return this;
+};
+
+/**
+ * Set global handlebar input data.
+ */
+const setLocals = function(locals = {}) {
+  _locals = locals;
+};
+
+/**
+ * Set options.
+ *   cache: boolean | cache template data
+ */
+const setOptions = function(options = {}) {
+  Object.assign(_options, options);
+};
+
+/**
+ * Get handlebar instances for subject and html body.
+ *   valueFn expects to be an asynchronous function returning template, subject.
+ *   template expects to be an template string.
+ *   templatePath expects to be an url of the template 
+ */
+const getTemplate = function({ template, templatePath, subject, valueFn }) {
+  if (valueFn) {
+    return valueFn().then(({ template, subject }) => {
+      return {
+        subject: Handlebars.compile(subject),
+        html: Handlebars.compile(template)
+      };
+    });
+  } else {
+    const templateProm = template ?
+      Promise.resolve(template) :
+      readFileAsync(templatePath).catch(err => {
+        console.error(err);
+        return 'no template';
+      });
+
+    return templateProm.then(template => {
+      return {
+        subject: Handlebars.compile(subject),
+        html: Handlebars.compile(template)
+      };
+    });
+  }
+};
+
+/**
+ * Register a single template.
+ *   if cache option is set to true, store handlebar instances for subject and html body.
+ *   if not, create handlebar instances as sending emails.
+ */
+const registerTemplate = function({ key, template, templatePath, subject, valueFn }) {
+  if (_options.cache) {
+    return getTemplate({ template, templatePath, subject, valueFn })
+      .then(({ subject, html }) => {
+        _cached[key] = { subject, html };
+        return _cached[key];
+      });
+
+  } else {
+    _templates[key] = { template, templatePath, subject, valueFn };
+    return Promise.resolve(_templates[key]);
+  }
+};
+
+/**
+ * Register a single template or more than one templates.
+ */
+const registerTemplates = function(templates) {
+  return isArray(templates) ?
+    Promise.all(templates.map(registerTemplate)) :
+    registerTemplate(templates);
+};
+
+/**
+ * Send an email using node mailer.
+ *   in this state, subject and html body are interpolated.
+ */
+const send = function({
+  from = 'sender@example.com', // sender address
+  to = 'receiver1@example.com, receiver2@example.com', // list of receivers
+  subject = 'Hello There', // Subject line
+  html = '<b>Hello There?</b>', // html body
+  text = htmlToText.fromString(html, { wordwrap: 130 }), // plain text body
+}) {
+  return new Promise((resolve, reject) => {
+    if (!_smtpTransport) return reject(new Error('plz create a smtp transport first! hbs-mailer.createTransport(opts)...'))
+
+    // disable for now untile email works!
+    _smtpTransport.sendMail({ from, to, subject, text, html }, (err, info) => {
+      if (err) reject(err);
+      else {
+        info.subject = subject;
+        info.html = html;
+        info.text = text;
+        resolve(info);
+      }
+    });
+  });
+};
+
+/**
+ * Interpolate subject and html body with input data and send email.
+ */
+const sendEmail = function({ key, data, receiver, sender, extraData }) {
+  const templateProm = _options.cache ? Promise.resolve(_cached[key]) : getTemplate(_templates[key]);
+
+  return templateProm.then(template => {
+    if (!template) throw new Error(`template ${key} is not registered!`);
+
+    const locals = Object.assign({}, { receiver, sender }, _locals, data, extraData);
+    const subject = template.subject(locals);
+    const html = template.html(locals);
+
+    if (!sender) sender = locals.sender;
+    if (!sender) return Promise.reject(new Error('sender information is not provided!'));
+
+    const options = {
+      from: sender.email || sender,
+      to: receiver.email || receiver,
+      subject,
+      html
+    };
+
+    return send(options);
+  });
+};
+
+/**
+ * Bind request data to handlebar input data and create a method in request.
+ */
+const bind = function(transportOptions) {
+  if (transportOptions) createTransport(transportOptions);
+
+  return function(req, res, next) {
+    req.sendEmail = function({ key, data, receiver, sender }) {
+      const extraData = {
+        app: req.app.locals,
+        req: {
+          domain: req.domain,
+          protocol: req.protocol,
+          hostname: req.hostname,
+          ip: req.ip,
+          baseUrl: req.baseUrl,
+          originalUrl: req.originalUrl,
+          path: req.path,
+          body: req.body,
+          query: req.query,
+          params: req.params,
+          headers: req.headers,
+          httpVersion: req.httpVersion,
+        }
+      };
+      return sendEmail({ key, data, receiver, sender, extraData });
+    }
+    next()
+  };
+};
+
+/**
+ * Export methods.
+ */
+module.exports = exports = {
+  createTransport,
+  setLocals,
+  setOptions,
+  registerTemplates,
+  sendEmail,
+  bind,
+};
---
+++ test.js
@@ -0,0 +1,269 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const nodemailer = require("nodemailer");
+const axios = require('axios');
+const assert = require('assert');
+
+const mailer = require('./index');
+mailer.setOptions({ cache: false });
+
+const port = 8080;
+const baseUrl = `http://localhost:${port}`;
+
+const app = express();
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+
+let testAccount;
+const receiver = 'test1@test.com';
+const sender = '"Test" <admin@test.com>';
+
+describe('Create Test Account', function() {
+  this.timeout(5000);
+
+  it(`should create a test account`, function(done) {
+    nodemailer.createTestAccount().then(account => {
+      testAccount = account;
+      done();
+    });
+  });
+});
+
+describe('Create Transport', function() {
+  this.timeout(5000);
+
+  it(`should create nodemailer transport`, function(done) {
+    mailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass // generated ethereal password
+      }
+    });
+    done();
+  });
+});
+
+describe('Register Template', function() {
+  this.timeout(5000);
+
+  it(`should register a template`, function(done) {
+    const _template = '<p>Hello World!</p>';
+    mailer.registerTemplates([{
+      key: 'test1',
+      template: _template,
+      subject: 'Hello World',
+    }, {
+      key: 'test2',
+      template: '<p>{{fruit}}</p>',
+      subject: 'fruit',
+    }, {
+      key: 'test3',
+      template: '<p>{{req.path}}</p>',
+      subject: 'request path',
+    }]).then(templates => {
+      const ass = assert.equal(_template, templates[0].template);
+      done(ass);
+    });
+  });
+});
+
+describe('Send Email', function() {
+  this.timeout(5000);
+
+  it(`should accept ${receiver}`, function(done) {
+    mailer.sendEmail({
+        key: 'test1',
+        receiver,
+        sender
+      })
+      .then(info => {
+        assert.equal(info.accepted[0], receiver);
+        done();
+      });
+  });
+});
+
+describe('Send Email with Interpolated HTML', function() {
+  this.timeout(5000);
+
+  it(`should replace {{fruit}} to 'apple'`, function(done) {
+    mailer.sendEmail({
+        key: 'test2',
+        data: { fruit: 'apple' },
+        receiver,
+        sender
+      })
+      .then(info => {
+        assert.equal(info.text, 'apple');
+        done();
+      });
+  });
+});
+
+describe('Send Email with Local Data', function() {
+  this.timeout(5000);
+
+  mailer.setLocals({ fruit: 'apple' });
+  it(`should replace {{fruit}} to 'apple'`, function(done) {
+    mailer.sendEmail({
+        key: 'test2',
+        receiver,
+        sender
+      })
+      .then(info => {
+        assert.equal(info.text, 'apple');
+        done();
+      });
+  });
+});
+
+describe('Fail to Send Email with Request', function() {
+  this.timeout(5000);
+
+  const path = 'send-email-with-request';
+
+  it(`should have no 'sendEmail' method in request`, function(done) {
+    app.get(`/${path}`, function(req, res, next) {
+      assert.equal(req.sendEmail, undefined);
+      done();
+    });
+    axios.get(`${baseUrl}/${path}`);
+  });
+});
+
+describe('Create Transport and Bind to request', function() {
+  this.timeout(5000);
+
+  it(`should create nodemailer transport`, function(done) {
+    app.use(mailer.bind({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass // generated ethereal password
+      }
+    }));
+    done();
+  });
+});
+
+describe('Send Email with Request Data', function() {
+  this.timeout(5000);
+
+  const path = '/send-email-with-request-data';
+
+  it(`should replace {{req.path}} to '/${path}'`, function(done) {
+    app.get(`${path}`, function(req, res, next) {
+      req.sendEmail({
+          key: 'test3',
+          receiver,
+          sender
+        })
+        .then(info => {
+          assert.equal(info.text, path);
+          done();
+        });
+    });
+    axios.get(`${baseUrl}${path}`);
+  });
+});
+
+describe('Register Template with Cache', function() {
+  this.timeout(5000);
+
+  it(`should cache a template`, function(done) {
+    mailer.setOptions({ cache: true });
+
+    const _template = '<p>Hello {{World}} Again!</p>';
+    mailer.registerTemplates({
+      key: 'test4',
+      template: _template,
+      subject: 'Hello World Again',
+    }).then(({ html }) => {
+      // handlebar instance
+      assert.equal('<p>Hello  Again!</p>', html());
+      done();
+    });
+  });
+
+  it(`should replace {{World}} to 'kitty'`, function(done) {
+    mailer.sendEmail({
+        key: 'test4',
+        data: { World: 'kitty' },
+        receiver,
+        sender
+      })
+      .then(info => {
+        assert.equal(info.text, 'Hello kitty Again!');
+        done();
+      });
+  });
+});
+
+describe('Register Template with Value Function', function() {
+  this.timeout(5000);
+
+  it(`should cache a template`, function(done) {
+
+    const _template = '<p>Hello {{World}} Again!</p>';
+    mailer.registerTemplates({
+      key: 'test5',
+      valueFn: () => Promise.resolve({ template: '<p>I am a {{animal}}</p>', subject: 'Who are you?' }),
+    }).then(({ html }) => {
+      // handlebar instance
+      assert.equal('<p>I am a </p>', html());
+      done();
+    });
+  });
+
+  it(`should replace {{animal}} to 'cat'`, function(done) {
+    mailer.sendEmail({
+        key: 'test5',
+        data: { animal: 'cat' },
+        receiver,
+        sender
+      })
+      .then(info => {
+        assert.equal(info.text, 'I am a cat');
+        done();
+      });
+  });
+});
+
+describe('Register Template with Value Template Path', function() {
+  this.timeout(5000);
+
+  it(`should cache a template`, function(done) {
+
+    const _template = '<p>Hello {{World}} Again!</p>';
+    mailer.registerTemplates({
+      key: 'test6',
+      templatePath: path.resolve('./test.html'),
+      subject: 'Hello There'
+    }).then(({ html }) => {
+      // handlebar instance
+      assert.equal('<p>which </p>', html());
+      done();
+    });
+  });
+
+  it(`should replace {{animal}} to 'cat'`, function(done) {
+    mailer.sendEmail({
+        key: 'test6',
+        data: { fruit: 'banana' },
+        receiver,
+        sender
+      })
+      .then(info => {
+        assert.equal(info.text, 'which banana');
+        done();
+      });
+  });
+});
